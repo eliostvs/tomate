05c53b74bc6534d3170af6bdf8d5478eaa528155
Elio E. Duarte <elio.esteves.duarte@gmail.com>
2015-01-31 22:09:11 -0200

    remove unused functions

--- tomate/utils.py
+++ tomate/utils.py
@@ -2,7 +2,6 @@ from __future__ import unicode_literals
 
 import functools
 import logging
-import os
 
 
 def setup_logging(options):
@@ -33,31 +32,6 @@ def suppress_errors(function):
             logger.error(e, exc_info=True)
 
     return wrapper
-
-
-def find_xdg_data_files(install_path, topdir, pkgname, data_files=[]):
-    for (dirpath, _, filenames) in os.walk(topdir):
-        if filenames:
-            install_path = install_path.format(pkgname=pkgname)
-
-            subpath = dirpath.split(topdir)[1]
-            if subpath.startswith('/'):
-                subpath = subpath[1:]
-
-            files = [os.path.join(dirpath, f) for f in filenames]
-
-            data_files.append((os.path.join(install_path, subpath), files))
-
-    return data_files
-
-
-def find_data_files(data_map, pkgname):
-    data_files = []
-
-    for (system_path, local_path) in data_map:
-        find_xdg_data_files(system_path, local_path, pkgname, data_files)
-
-    return data_files
 
 
 class fsm(object):
