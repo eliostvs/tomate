a77b40280afdf8a94b62f480da36ec85fb1ab6fa
Elio E. Duarte <elio.esteves.duarte@gmail.com>
2015-02-02 08:59:24 -0200

    refactoring

--- tomate/constants.py
+++ tomate/constants.py
@@ -4,13 +4,16 @@ import enum
 
 
 @enum.unique
-class Task(enum.Enum):
-    pomodoro = 0
-    shortbreak = 1
-    longbreak = 2
+class BaseEnum(enum.Enum):
 
     @classmethod
     def get_by_index(cls, index):
         for i, k in enumerate(cls):
             if i == index:
                 return k
+
+
+class Task(BaseEnum):
+    pomodoro = 0
+    shortbreak = 1
+    longbreak = 2
