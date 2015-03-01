9e80e577ab78e7e895806c264e76a1514d8bd7bb
Elio E. Duarte <elio.esteves.duarte@gmail.com>
2015-02-28 22:09:03 -0300

    Removed InjectablePluginManager

--- tomate/plugin.py
+++ tomate/plugin.py
@@ -2,27 +2,8 @@ from __future__ import unicode_literals
 
 from wiring import Module, provides, scope, SingletonScope
 from yapsy.IPlugin import IPlugin
-from yapsy.PluginManagerDecorator import PluginManagerDecorator
 
 from .signals import Subscriber
-
-
-class InjectablePluginManager(PluginManagerDecorator):
-
-    def __init__(self, decorated_manager=None):
-        super(InjectablePluginManager, self).__init__(decorated_manager)
-        self._graph = None
-
-    def loadPlugins(self, callback=None):
-        processed_plugins = self._component.loadPlugins(callback)
-
-        for plugin in processed_plugins:
-            plugin.plugin_object.graph = self._graph
-
-        return processed_plugins
-
-    def setGraph(self, graph):
-        self._graph = graph
 
 
 class Plugin(Subscriber, IPlugin):
@@ -41,17 +22,13 @@ class PluginProvider(Module):
     @provides('tomate.plugin')
     @scope(SingletonScope)
     def create_plugin_manager(self):
-        from tomate.graph import graph
         from yapsy.ConfigurablePluginManager import ConfigurablePluginManager
         from yapsy.PluginManager import PluginManagerSingleton
         from yapsy.VersionedPluginManager import VersionedPluginManager
 
         PluginManagerSingleton.setBehaviour([
-            InjectablePluginManager,
             ConfigurablePluginManager,
             VersionedPluginManager,
         ])
 
-        manager = PluginManagerSingleton.get()
-        manager.setGraph(graph)
-        return manager
+        return PluginManagerSingleton.get()
