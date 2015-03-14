49c3ccc85d2e70af632b68dacfd4b715fe7046eb
Elio E. Duarte <elio.esteves.duarte@gmail.com>
2015-03-14 18:07:36 -0300

    update version

--- setup.py
+++ setup.py
@@ -14,6 +14,6 @@ setup(
     name='tomate',
     packages=find_packages(exclude=['*tests']),
     url='https://github.com/eliostvs/tomate',
-    version='0.2.0',
+    version='0.2.1',
     zip_safe=False,
 )
