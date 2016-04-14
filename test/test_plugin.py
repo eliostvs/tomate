cef54271bfc5c014889a12ef82e83d80752ae601
Elio E. Duarte <elio.esteves.duarte@gmail.com>
2016-04-13 22:07:11 -0300

    Using py.test in test_plugin

--- test/test_plugin.py
+++ test/test_plugin.py
@@ -1,6 +1,6 @@
 from __future__ import unicode_literals
 
-import unittest
+import pytest
 
 from mock import Mock
 from wiring import FactoryProvider, SingletonScope
@@ -10,53 +10,58 @@ from tomate.event import on
 from tomate.plugin import Plugin
 
 
-class TestPlugin(unittest.TestCase):
-
-    def setUp(self):
-        session = Mock()
-        timer = Mock()
-
-        class Foo(Plugin):
-            @on(session, [State.finished])
-            def bar(self, sender):
-                return sender
-
-            @on(timer, [State.finished, State.changed])
-            def spam(self, sender):
-                return sender
-
-        self.foo = Foo()
-        self.timer_event = timer
-        self.session_event = session
-
-    def test_should_disconnect_events_when_plugin_deactive(self):
-        self.foo.deactivate()
-
-        self.timer_event.disconnect.assert_any_call(self.foo.spam, sender=State.changed)
-        self.timer_event.disconnect.assert_any_call(self.foo.spam, sender=State.finished)
-
-    def test_should_connect_events_when_plugin_active(self):
-        self.foo.activate()
-
-        self.timer_event.connect.assert_any_call(self.foo.spam, sender=State.changed, weak=False)
-        self.timer_event.connect.assert_any_call(self.foo.spam, sender=State.finished, weak=False)
+@pytest.fixture()
+def timer():
+    return Mock()
 
 
-class TestPluginModule(unittest.TestCase):
+@pytest.fixture()
+def session():
+    return Mock()
 
-    def test_module(self):
-        from yapsy.PluginManagerDecorator import PluginManagerDecorator
-        from tomate.plugin import PluginModule
-        from tomate.graph import graph
 
-        PluginModule().add_to(graph)
+@pytest.fixture()
+def foo(timer, session):
+    class Foo(Plugin):
+        @on(session, [State.finished])
+        def bar(self, sender):
+            return sender
 
-        provider = graph.providers['tomate.plugin']
-        self.assertIn('tomate.plugin', graph.providers.keys())
+        @on(timer, [State.finished, State.changed])
+        def spam(self, sender):
+            return sender
 
-        self.assertIsInstance(provider, FactoryProvider)
-        self.assertEqual(provider.scope, SingletonScope)
-        self.assertEqual(provider.dependencies, {})
+    return Foo()
 
-        plugin_manager = graph.get('tomate.plugin')
-        self.assertIsInstance(plugin_manager, PluginManagerDecorator)
+
+def test_should_disconnect_events_when_plugin_deactive(foo, timer):
+    foo.deactivate()
+
+    timer.disconnect.assert_any_call(foo.spam, sender=State.changed)
+    timer.disconnect.assert_any_call(foo.spam, sender=State.finished)
+
+
+def test_should_connect_events_when_plugin_active(foo, timer):
+    foo.activate()
+
+    timer.connect.assert_any_call(foo.spam, sender=State.changed, weak=False)
+    timer.connect.assert_any_call(foo.spam, sender=State.finished, weak=False)
+
+
+def test_module():
+    from yapsy.PluginManagerDecorator import PluginManagerDecorator
+    from tomate.plugin import PluginModule
+    from tomate.graph import graph
+
+    PluginModule().add_to(graph)
+
+    provider = graph.providers['tomate.plugin']
+
+    assert 'tomate.plugin' in graph.providers.keys()
+
+    assert isinstance(provider, FactoryProvider)
+    assert provider.scope == SingletonScope
+    assert provider.dependencies == {}
+
+    plugin_manager = graph.get('tomate.plugin')
+    assert isinstance(plugin_manager, PluginManagerDecorator)
