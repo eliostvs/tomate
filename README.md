fd7dc7961efd69d79c42668dc7294db9ad75cb02
Elio Duarte <eduarte@thoughtworks.com>
2019-02-28 16:44:36 -0300

    Bump version: 0.11.0 → 0.12.0

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ Bugs and suggestions should be reported [here][2].
 Changelog
 ---------
 
-### [Unreleased]
+### 0.12.0
 
 #### Added
 
--- setup.py
+++ setup.py
@@ -14,6 +14,6 @@ setup(
     name="tomate",
     packages=find_packages(exclude=["*tests"]),
     url="https://github.com/eliostvs/tomate",
-    version="0.11.0",
+    version="0.12.0",
     zip_safe=False,
 )
