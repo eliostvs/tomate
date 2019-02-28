e8795ee2fa7a4bc03d2cb5de0f6334f48692f476
Elio Duarte <eduarte@thoughtworks.com>
2019-02-28 10:23:50 -0300

    Using black formatter.

--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@ from setuptools import find_packages, setup
 
 
 setup(
-    author='Elio Esteves Duarte',
-    author_email='elio.esteves.duarte@gmail.com',
-    description='A pomodoro timer. Core classes.',
+    author="Elio Esteves Duarte",
+    author_email="elio.esteves.duarte@gmail.com",
+    description="A pomodoro timer. Core classes.",
     include_package_data=True,
-    keywords='pomodoro',
-    license='GPL-3',
-    long_description=open('README.md').read(),
-    name='tomate',
-    packages=find_packages(exclude=['*tests']),
-    url='https://github.com/eliostvs/tomate',
-    version='0.11.0',
+    keywords="pomodoro",
+    license="GPL-3",
+    long_description=open("README.md").read(),
+    name="tomate",
+    packages=find_packages(exclude=["*tests"]),
+    url="https://github.com/eliostvs/tomate",
+    version="0.11.0",
     zip_safe=False,
 )
